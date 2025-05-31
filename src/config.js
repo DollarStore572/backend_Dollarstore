4623c8c768b2342c0785924aaeff937ca4d34102
EdgarMrt26 <edgarmartinez09m@gmail.com>
2025-05-30 21:19:50 -0600

    Integración de IA en los gráficos estadísticos.

--- src/config.js
+++ src/config.js
@@ -2,16 +2,19 @@ import {config} from 'dotenv';
 
 config();
 
-
 export const PORT = process.env.PORT || 3000;
 export const DB_HOST = process.env.DB_HOST || 'localhost';
-export const DB_PORT = process.env.DB_PORT || 3307;
-export const DB_USER = process.env.DB_USER || 'dbgustavo';
-export const DB_PASSWORD = process.env.DB_PASSWORD || 'messi12';
-export const DB_DATABASE = process.env.DB_DATABASE || 'dollarstore_act';
+export const DB_PORT = process.env.DB_PORT || 3306;
+export const DB_USER = process.env.DB_USER || 'root';
+export const DB_PASSWORD = process.env.DB_PASSWORD || '123456';
+export const DB_DATABASE = process.env.DB_DATABASE || 'dollarstore_ACT';
 
-
-
+export const PORT2 = process.env.PORT2 || 3000;
+export const DB_HOST2 = process.env.DB_HOST2 || 'localhost';
+export const DB_PORT2 = process.env.DB_PORT2 || 3306;
+export const DB_USER2 = process.env.DB_USER2 || 'root';
+export const DB_PASSWORD2 = process.env.DB_PASSWORD2 || '123456';
+export const DB_DATABASE2 = process.env.DB_DATABASE2 || 'dollarstore_ACT_datamart';
 
 //export const PORT = process.env.PORT;
 //export const DB_HOST = process.env.DB_HOST;
