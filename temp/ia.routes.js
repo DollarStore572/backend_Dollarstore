ed9a63fbfbe8bc8e9a299c01b7451462d164ede9
Gustao123 <gustavoeominguez80@gmail.com>
2025-06-01 19:46:21 -0600

    Actualizacion del Backend

---
+++ temp/ia.routes.js
@@ -0,0 +1,12 @@
+// routes/ia.routes.js
+import { Router } from 'express';
+import { consultarConIA } from '../controllers/ia.controller.js';
+
+const router = Router();
+
+router.post('/consultarconia', consultarConIA);
+
+export default router;
+
+
+
